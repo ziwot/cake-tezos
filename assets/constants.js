bc58a5eff6458e1b4dc4b0e52904c736943cdba8
Steven J. <sjehann@gmail.com>
2026-04-21 00:07:05 +0200

    migrate to octez.connect

---
+++ assets/constants.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2022, vDL Digital Ventures GmbH <info@vdl.digital>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+export const TEZOS_SIGNED_MESSAGE_PREFIX = "Tezos Signed Message: ";
+
+export const SIGN_IN_MESSAGE = "wants you to sign in with your Tezos account:";
+
+export const OPTIONAL_MESSAGE_PROPERTIES = {
+    uri: "Uri",
+    version: "Version",
+    chainId: "Chain ID",
+    nonce: "Nonce",
+    issuedAt: "Issued At",
+    expirationTime: "Expiration Time",
+    notBefore: "Not Before",
+    requestId: "Request ID",
+};
---
+++ assets/http.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2022, vDL Digital Ventures GmbH <info@vdl.digital>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+const fetchWithTimeout = async (resource, userOptions) => {
+    const defaultOptions = {
+        timeout: 3000,
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    const options = { ...defaultOptions, ...userOptions };
+
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), options.timeout);
+
+    try {
+        const response = await fetch(resource, {
+            ...options,
+            signal: controller.signal,
+        });
+        const data = await response.json();
+        return { data };
+    } catch (_error) {
+        throw new Error("Fetching failed");
+    } finally {
+        clearTimeout(id);
+    }
+};
+
+export const http = fetchWithTimeout;
